33a907155b88b66d497c496c6a81c453601a42d1
Waylan Garton <75507050+Waylan219@users.noreply.github.com>
2021-11-22 14:59:48 -0600

    Update filterByGenre.R

--- filterByGenre.R
+++ filterByGenre.R
@@ -1,4 +1,7 @@
-# two while loops to loop through entire database and then the entries in the genre category
+# syntax      : newDataFrame = filterByGenre(testing,genre = "Comedy")
+# testing     : dataframe you want to parse through , only use the dataframe with the movies and not actors
+# genre       : a string of the genre you want to have
+# newDataFrame : the new database you wish to create with only rows containing the genre you passed
 filterByGenre <- function(dataframe,genre){
   newdataframe <- subset(dataframe,FALSE)
   genrelist <- strsplit(dataframe$genre,',')
